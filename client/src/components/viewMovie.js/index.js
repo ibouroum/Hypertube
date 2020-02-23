37153fbf08f6f82ecb4201e75e4a759d3df79c59
Ismail Bouroummana <ibouroum@e2r6p2.1337.ma>
2020-02-23 15:25:23 +0100

    Working on movie page

--- client/src/components/viewMovie.js/index.js
+++ client/src/components/viewMovie.js/index.js
@@ -1,32 +1,83 @@
 import React from 'react'
-import Avatar from '@material-ui/core/Avatar';
 import './index.css';
-import { DialogContent, Grid } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import PlayCircleFilledWhiteIcon from '@material-ui/icons/PlayCircleFilledWhite';
-import SystemUpdateAltIcon from '@material-ui/icons/SystemUpdateAlt';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
-import ListItemAvatar from '@material-ui/core/ListItemAvatar';
-import WorkIcon from '@material-ui/icons/Work';
-import BeachAccessIcon from '@material-ui/icons/BeachAccess';
-import Divider from '@material-ui/core/Divider';
 import Typography from '@material-ui/core/Typography';
 import Rating from '@material-ui/lab/Rating';
+import Grid from '@material-ui/core/Grid';
+import GridList from '@material-ui/core/GridList';
+import GridListTile from '@material-ui/core/GridListTile';
+import GridListTileBar from '@material-ui/core/GridListTileBar';
+import IconButton from '@material-ui/core/IconButton';
+import StarBorderIcon from '@material-ui/icons/StarBorder';
+const tileData = [
+    {img : "https://picsum.photos/200/300?grayscale",title : "ismail"},
+    {img : "https://picsum.photos/200/300?grayscale",title : "ismail"},
+    {img : "https://picsum.photos/200/300?grayscale",title : "ismail"},
+    {img : "https://picsum.photos/200/300?grayscale",title : "ismail"},
+    {img : "https://picsum.photos/200/300?grayscale",title : "ismail"},
+    {img : "https://picsum.photos/200/300?grayscale",title : "ismail"},
+
+    {img : "https://picsum.photos/200/300?grayscale",title : "ismail"},
+    {img : "https://picsum.photos/200/300?grayscale",title : "ismail"},
+
+    {img : "https://picsum.photos/200/300?grayscale",title : "ismail"},
+    {img : "https://picsum.photos/200/300?grayscale",title : "ismail"},
+];
+const useStyles = makeStyles(theme => ({
+    gridList: {
+      flexWrap: 'nowrap',
+      height  :200,
+      maxWidth:1200
+      //transform: 'translateZ(0)',
+    },
+  }));
 const ViewMovie = () => {
+    const classes = useStyles();
     return (
-      
-        <div class="container">
+      <div class="root">
+      <Grid container justify="center" >
+      <Typography align="center">Titre</Typography>
+        <Grid item  justify="center" xs={12} sm={6}>
             <img class ="image" alt="images" src="https://picsum.photos/200/300?grayscale"/>
-            <div class="overlay">
-                <PlayCircleFilledWhiteIcon class="icone"/>
-            </div>
-            <Rating  defaultValue={2.5} precision={0.5} readOnly />
-          <Typography>Inception</Typography>
+            <div class="overlay"><PlayCircleFilledWhiteIcon class="icone"/></div>
+            
+        </Grid>
+        <Grid item justify="center" xs={12} sm={6}>
+        <Rating  defaultValue={2.5} precision={0.5} readOnly />
+            
+            <Typography>Inception</Typography>
             <Typography>Genre</Typography>
             <Typography>Année de production</Typography>
             <Typography>Durée</Typography>
-            </div>
+        </Grid>
+        <Grid item xs={12} >
+          <GridList className={classes.gridList} cols={4}>
+              {tileData.map(tile => (
+                  <GridListTile key={tile.img}>
+                      <img class="img" src={tile.img} alt={tile.title} />
+                      <GridListTileBar
+                          title={tile.title}
+                      />
+                  </GridListTile>
+              ))}
+          </GridList>
+        </Grid>
+        <Grid item xs={12} >
+          <GridList className={classes.gridList} cols={4}>
+              {tileData.map(tile => (
+                  <GridListTile key={tile.img}>
+                      <img class="img" src={tile.img} alt={tile.title} />
+                      <GridListTileBar
+                          title={tile.title}
+                      />
+                  </GridListTile>
+              ))}
+          </GridList>
+        </Grid>
+      </Grid>
+      </div>
+      
     )
 }
 
